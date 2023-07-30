b25071a9c62bebef6e71e27a644308ff5ae4ff08
resire02 <65257747+resire02@users.noreply.github.com>
2023-07-29 17:01:39 -0700

    added milestone panel

--- Assets/Scripts/Event/MilestoneEventHandler.cs
+++ Assets/Scripts/Event/MilestoneEventHandler.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(RandomEventHandler))]
 public class MilestoneEventHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject milestonePanel;
+    private ImageLoader imgLd;
+    
+    private void Start()
+    {
+        milestonePanel.SetActive(false);
+        imgLd = GetComponent<ImageLoader>();
+    }
+
     public void TriggerMilestoneEvent(Age age)
     {
         Debug.Log("Triggered Milestone Event");
 
         //  TODO: prompt milestone event (no option), play cutscene?
+        imgLd.SetImage("MilestonePlaceholder");
+        milestonePanel.SetActive(true);
     }
 }
--- Assets/Scripts/ImageLoader.cs
+++ Assets/Scripts/ImageLoader.cs
@@ -5,18 +5,26 @@ using UnityEngine.UI;
 
 public class ImageLoader : MonoBehaviour
 {
-    public Image background;
+    [SerializeField] private Image background;
+    [SerializeField] private Image transition;
     private Sprite currentSprite;
+    private bool inTransition;
 
     private void Start()
     {
         SetImage("CaveBackground");
     }
 
+    private void FixedUpdate()
+    {
+        if(!inTransition) return;
+    }
+
     //  files need to be in `Resources/Image`
     public void SetImage(string filename)
     {
+        inTransition = true;
         currentSprite = Instantiate(Resources.Load<Sprite>($"Image/{filename}"));
-        background.sprite = currentSprite; 
+        background.sprite = currentSprite;
     }
 }
