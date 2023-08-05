030a664755ff15711b3cdb72733e5213aa9aa2c7
resire02 <65257747+resire02@users.noreply.github.com>
2023-08-05 00:04:33 -0700

    fixed event duplication bug

--- Assets/Scripts/GameUI/GameEndMenu.cs
+++ Assets/Scripts/GameUI/GameEndMenu.cs
@@ -10,20 +10,26 @@ public class GameEndMenu : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI yearText;
     [SerializeField] private AudioHandler music;
+    [SerializeField] private TextMeshProUGUI attempts;
 
     private void Start()
     {
         gameOverMenu.SetActive(false);
-        RandomEventList.InitializeAllEvents();
     }
 
     public void TriggerGameEnd(float score)
     {
         gameOverMenu.SetActive(true);
+
+        AttemptCounter.IncrementAttempts();
+
         music.PlaySound("End", true);
         music.StopSound("Game");
+
         scoreText.text = $"Civilization Score: {score}";
         yearText.text = $"Civilizaton Lasted: {gameLogic.GetComponent<RandomEventHandler>().GetYear()} Years";
+        attempts.text = $"Civilizations Collapsed: {AttemptCounter.attempts}";
+
     }
 
     public void ResetGame()
@@ -34,6 +40,7 @@ public class GameEndMenu : MonoBehaviour
         gameLogic.GetComponent<RandomEventHandler>().Reset();
         gameLogic.GetComponent<MilestoneEventHandler>().Reset();
 
+        RandomEventList.InitializeAllEvents();
         music.PlaySound("Game", true);
         music.StopSound("End");
         gameOverMenu.SetActive(false);
