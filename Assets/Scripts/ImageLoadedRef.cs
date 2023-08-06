2e88297944c2f7f337b62b5f0130d8efbe261ef6
Jason <Jason231.az@gmail.com>
2023-08-05 20:17:28 -0700

    added location for sprites

--- Assets/Scripts/ImageLoadedRef.cs
+++ Assets/Scripts/ImageLoadedRef.cs
@@ -33,7 +33,14 @@ public static class ImageLoadedRef
         //  TODO: add scene data for all ages excluding extinct
         sceneData.Add(Age.Prehistoric, ("PrehistoricWithSky", "PrehistoricBase"));
         sceneData.Add(Age.Civilizing, ("Civilization", "CivilizationBase"));
+        //sceneData.Add(Age.Medieval, ("Medieval", "Medieval"));
+        // sceneData.Add(Age.Colonial, ("Colonial", "Colonial"));
+        // sceneData.Add(Age.Industrial, ("Industrial", "Industrial"));
+        // sceneData.Add(Age.Information, ("Information", "Information"));
+        sceneData.Add(Age.Space, ("Space", "Space"));
         sceneData.Add(Age.Cosmic, ("Cosmic", "Cosmic"));
+        //sceneData.Add(Age.Galactic, ("Galactic", "Galactic"));
+        //sceneData.Add(Age.Singularity, ("Singularity", "Singularity"));
 
     }
 
